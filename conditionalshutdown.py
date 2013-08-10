a24705138e5dc1d708129dd372fb050adf8be9c4
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2013-08-10 03:43:04 +0200

    Implement functionality for monitory hosts for a certain time period before invoking shutdown.

--- conditionalshutdown.py
+++ conditionalshutdown.py
@@ -23,16 +23,19 @@
 
 import os
 import sys
-from subprocess import Popen, PIPE
+import time
 import logging
+from subprocess import Popen, PIPE
 from logging.handlers import RotatingFileHandler
 
 """
 Invoke shutdown if none of a list of specified hosts is reachable over the
-network. Reachability is tested via ICMP requests (pings).
+network during a certain time period (a couple of minutes, usually).
+Reachability is tested via ICMP requests (pings) in regular intervals (a couple
+of seconds, usually).
 
 On FreeBSD as well as Linux the exit code of the ping program can be used for
-determining if a host is alive. QUote from FreeBSD's ping manual:
+determining if a host is alive. Quote from FreeBSD's ping manual:
 
     EXIT STATUS
          The ping utility exits with one of the following values:
@@ -41,12 +44,12 @@ determining if a host is alive. QUote from FreeBSD's ping manual:
 
 Here, `ping -o -t 5 host` is used. By default, ping sends a request once per
 second. `-t 5` makes sure that ping exits (latest) after 5 seconds. In other
-words, if the host does not immediately respond to the first request although it
-is online, a few more requests are sent. Any response arriving within 5 seconds
-after `ping` invocation is sufficient to detect the target host as being alive.
-`-o` makes `ping` exit upon the first reply. Assuming that the hosts to poll
-generally respond to ICMP requests, this configuration should be pretty safe and
-insensitive to minor network hiccups.
+words, if the host does not immediately respond to the first request although
+it is online, a few more requests are sent. Any response arriving within 5
+seconds after `ping` invocation is sufficient to detect the target host as
+being alive. `-o` makes `ping` exit upon the first reply. Assuming that the
+hosts to poll generally respond to ICMP requests, this configuration should be
+pretty safe and insensitive to minor network hiccups.
 """
 
 # List of hosts to check for being reachable. If name resolution is properly
@@ -54,25 +57,55 @@ insensitive to minor network hiccups.
 HOSTS_TO_CHECK = [
     "hostname1",
     "hostname2",
-    "192.168.1.5
+    "192.168.1.5"
     ]
 
 # Path to the log file of this script. The log file is automatically created,
 # regularly rotated (when having a size of about 500 kB) and appended to among
 # script invocations.
-logfile_path = "/mnt/disks/sshadmin_home/conditionalshutdown/logfile.log"
+#logfile_path = "/mnt/disks/sshadmin_home/conditionalshutdown/logfile.log"
+logfile_path = "logfile.log"
+
+
+# Condition for shutdown is that all test hosts have constantly not been
+# reachable within a couple of minutes (a case where we don't want the NAS to
+# shut down is e.g. when all test hosts are simultaneously restarting, i.e.
+# not reachable only once and for a short time). `REQUIRED_OFFLINE_SECONDS`
+# defines the time interval that all hosts need to be offline before shutdown
+# is invoked. Something like 5 minutes is recommended, i.e. 300 s.
+REQUIRED_OFFLINE_SECONDS = 40
+
+# `POLLING_INTERVAL_SECONDS` specifies how frequently the list of hosts should
+# be checked during the time interval specified above. Checking every thirty
+# seconds within five minutes would be a reasonable choice.
+POLLING_INTERVAL_SECONDS = 10
+
+
+# Make sure that hosts are tested multiple times within
+# `REQUIRED_OFFLINE_SECONDS`.
+assert REQUIRED_OFFLINE_SECONDS > 2*POLLING_INTERVAL_SECONDS
 
 
 def main():
-    log.info("Pinging hosts...")
+    exit_if_any_host_up()
+    log.info("No host is reachable. Poll again, every %s s.",
+        POLLING_INTERVAL_SECONDS)
+    deadline = time.time() + REQUIRED_OFFLINE_SECONDS
+    deadline_str = time.strftime("%H:%M:%S", time.localtime(deadline))
+    while time.time() < deadline:
+        log.info('Invoke shutdown if no host comes up until %s.', deadline_str)
+        time.sleep(POLLING_INTERVAL_SECONDS)
+        exit_if_any_host_up()
+    log.info("'shutdown -p now' returncode: %s" %
+        run_subprocess(['/sbin/shutdown', '-p', 'now']))
+
+
+def exit_if_any_host_up():
+    log.info("Pinging hosts, exit program if one is up.")
     for host in HOSTS_TO_CHECK:
         if host_responding(host):
             log.info("Exit program.")
             sys.exit(0)
-    log.info("None of the listed hosts seems to be up. Invoke shutdown.")
-    print "PEEEEEP, shutdown"
-    log.info("'shutdown -p now' returncode: %s" %
-        run_subprocess(['/sbin/shutdown', '-p', 'now']))
 
 
 def host_responding(host):
